6eca0dd74576cccb5d7db3daf2a20d3781e89ab4
Ben <ben.eliav@campus.technion.ac.il>
2024-06-08 16:51:04 +0300

    added sweep for original transformer

---
+++ original_transformer_sweep.py
@@ -0,0 +1,32 @@
+import wandb
+
+YOUR_WANDB_USERNAME = "beneliav1"
+project = "Strategy_Transfer_TACL"
+
+command = [
+        "${ENVIRONMENT_VARIABLE}",
+        "${interpreter}",
+        "StrategyTransfer.py",
+        "${project}",
+        "${args}"
+    ]
+
+sweep_config = {
+    "name": "Recreates results for original transformer.",
+    "method": "grid",
+    "metric": {
+        "goal": "maximize",
+        "name": "ENV_Test_accuracy_per_mean_user_and_bot"
+    },
+    "parameters": {
+        "seed": {"values": list(range(3))},
+        "architecture": {"values": ["transformer"]},
+        "total_epochs": {"values": [10]},
+    },
+    "command": command
+}
+
+# Initialize a new sweep
+sweep_id = wandb.sweep(sweep=sweep_config, project=project)
+print("run this line to run your agent in a screen:")
+print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
